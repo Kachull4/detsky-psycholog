301665a0650e26baa9fd385aaf6e4e0035a79cb5
Kateřina Beranová <katerina.beranova4@gmail.com>
2025-04-23 14:50:22 +0200

    website redesign

---
+++ scripts.js
@@ -0,0 +1,14 @@
+const toggle = document.querySelector(".menu__toggle");
+const menu = document.querySelector(".menu__list");
+
+toggle.addEventListener("click", () => {
+  toggle.classList.toggle("is-active");
+  menu.classList.toggle("is-active");
+});
+
+document.querySelectorAll(".menu__link").forEach((link) => {
+  link.addEventListener("click", () => {
+    toggle.classList.remove("is-active");
+    menu.classList.remove("is-active");
+  });
+});
