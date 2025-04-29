fd9c9e34e530ccdf7fb2aba82a8971b673a795f7
Kateřina Beranová <katerina.beranova4@gmail.com>
2025-04-29 13:12:23 +0200

    accesibility

--- scripts.js
+++ scripts.js
@@ -2,6 +2,8 @@ const toggle = document.querySelector(".menu__toggle");
 const menu = document.querySelector(".menu__list");
 
 toggle.addEventListener("click", () => {
+  const expanded = toggle.getAttribute("aria-expanded") === "true" || false;
+  toggle.setAttribute("aria-expanded", !expanded);
   toggle.classList.toggle("is-active");
   menu.classList.toggle("is-active");
 });
